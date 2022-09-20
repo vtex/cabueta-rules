24b405196bee65fc935d62afc6928ab64d2d3aaf
gabriel-schneider-vtex <gabriel.schneider@vtex.com.br>
2022-09-20 11:49:27 -0300

    fixed curl

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws/ -H "Content-Type: application/json" -d "`cat $1`" | jq
+curl -s -X POST https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws/ -H "Content-Type: application/json" -d "`cat $1`"
