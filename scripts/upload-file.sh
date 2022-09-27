4084dbd95c5a52e19faf88ed2e2d127b09a6ac7c
gabriel-schneider-vtex <gabriel.schneider@vtex.com.br>
2022-09-27 13:21:23 -0300

    small fix to aws curl :

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -6,7 +6,7 @@
 
 #curl -X POST "https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws" --aws-sigv4 "$AWS_REGION" --user "$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY" -d "`cat $1`"
 
-awscurl -X POST "https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws" -d "`cat $1`"
+awscurl --service lambda -X POST "https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws" -d "`cat $1`"
 
 
 
