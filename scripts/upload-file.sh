d58a557567f28765b7ec84e657698578384ba338
gabriel-schneider-vtex <gabriel.schneider@vtex.com.br>
2022-09-27 11:53:39 -0300

    authorizing CURL

--- scripts/upload-file.sh
+++ scripts/upload-file.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-curl -s -X POST https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws/ -H "Content-Type: application/json" -d "`cat $1`"
+#curl -s -X POST https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws/ -H "Content-Type: application/json" -d "`cat $1`"
+
+#curl -X POST --aws-sigv4 $AWS_REGION --user "$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY" -H "Content-Type: application/json" -d "`cat $1`" http://gbrls.space
+
+curl -X POST "https://rdr6vopsh7g5fuzouihruw3bn40hwjjh.lambda-url.us-east-1.on.aws" --aws-sigv4 "$AWS_REGION" --user "$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY" -d "`cat $1`"
